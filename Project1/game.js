ff3a9498d8acab8e6438cc80f668b3cf14519891
tem73 <tem73@txstate.edu>
2018-10-15 00:00:59 -0500

    Added validatePiece function

--- Project1/game.js
+++ Project1/game.js
@@ -1,18 +1,23 @@
 var lastClicked;
-var gameBoard = createBoard(8,8,function(el,row,col,i){
-                            var selected = document.getElementById(i);
-                         console.log("You clicked on element:",el.id);
-                         console.log("You clicked on row:",row);
-                         console.log("You clicked on col:",col);
-                         console.log("You clicked on item #:",i);
-                         console.log("Cell value",selected.innerHTML);
+
+// Create an instance of the game board, and define in the function
+// that gets called when any cell is clicked
+var gameBoard = createBoard(function(el,row,col,i){
+        console.log("You clicked on element:",el.id);
+        console.log("You clicked on row:",row);
+        console.log("You clicked on col:",col);
+        console.log("You clicked on item #:",i);
+        console.log("Cell value:",el.innerHTML);
+        
+        console.log(validatePiece(i));
                          
-                         el.className='clicked';
-                         if (lastClicked) lastClicked.className='';
-                         lastClicked = el;
-                         });
+        el.className='clicked';
+        if (lastClicked) lastClicked.className='';
+        lastClicked = el;
+    });
 
 document.body.appendChild(gameBoard);
+
 
 var tx = document.createElement("P");
 tx.innerHTML = "Click me!";
@@ -22,51 +27,60 @@ tx.addEventListener("click",function(){
 tx.style.textAlign = 'center';
 document.body.appendChild(tx);
 
-//initialize game board
+// Initialize game board
 var boardState = [
-             ['','w','','w','','w','','w'],
-             ['w','','w','','w','','w',''],
-             ['','w','','w','','w','','w'],
-             ['','','','','','','',''],
-             ['','','','','','','',''],
-             ['b','','b','','b','','b',''],
-             ['','b','','b','','b','','b'],
-             ['b','','b','','b','','b','',]
-             ];
+    ['','w','','w','','w','','w'],
+    ['w','','w','','w','','w',''],
+    ['','w','','w','','w','','w'],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['b','','b','','b','','b',''],
+    ['','b','','b','','b','','b'],
+    ['b','','b','','b','','b','',]
+    ];
 
-console.log(gameBoard);
 drawBoard(boardState);
-console.log(boardState);
-console.log(gameBoard);
 
-function createBoard( rows, cols, callback ){
+
+function createBoard( callback ){
     var i=0;
     var board = document.createElement('table');
+    
     board.className = 'board';
     board.id = 'board';
     board.style.border = "thin solid #000";
-    for (var r=0;r<rows;++r){
+    
+    for (var r=0;r<8;++r){
         var tr = board.appendChild(document.createElement('tr'));
-        for (var c=0;c<cols;++c){
+        
+        for (var c=0;c<8;++c){
             var square = tr.appendChild(document.createElement('td'));
+            
             square.id = ++i;
             square.style.width = "40px";
             square.style.height = "40px";
             square.style.textAlign = "center";
             square.style.color = "white";
-            var data = square.appendChild(document.createTextNode('adsf'));
+            
+            var data = square.appendChild(document.createTextNode(''));
+            
             if(((c%2==0) && (r%2==1))||((c%2==1) && (r%2==0))) {
                 square.style.backgroundColor = "black";
             }
+            
+            // attatch event listener to each square, which calls
+            // a function that is defined when an instance of the
+            // board is created
             square.addEventListener('click',(function(el,r,c,i){
-                                            return function(){
-                                                callback(el,r,c,i);
-                                            }
-                                           })(square,r,c,i),false);
+                return function(){
+                callback(el,r,c,i);
+                }
+            })(square,r,c,i),false);
         }
     }
     return board;
 }
+
 
 function drawBoard(state) {
     var board = document.getElementById("board");
@@ -88,6 +102,21 @@ function drawBoard(state) {
             }
             a++;
         }
+    }
+}
+
+// Checks if the piece passed to the function
+// is the player's piece
+var validatePiece = function(el) {
+    var selected = document.getElementById(el);
+    // if they selected their piece, and it is not a king
+    if(selected.innerHTML == 'b' || selected.innerHTML == 'B') {
+        return true;
+    }
+    
+    // if they selected a square that does not have their piece
+    else {
+        return false;
     }
 }
 
