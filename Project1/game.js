cc1571099c1349b1cdc5a192d095aa3618e14e4b
tem73 <tem73@txstate.edu>
2018-10-14 19:17:43 -0500

    Replaced file with js file that uses tables

--- Project1/game.js
+++ Project1/game.js
@@ -1,90 +1,115 @@
-//Create players
-const PLAYER_1 = '⚪';
-const PLAYER_2 = '⚫';
+var lastClicked;
+var gameBoard = createBoard(8,8,function(el,row,col,i){
+                            var selected = document.getElementById(i);
+                         console.log("You clicked on element:",el.id);
+                         console.log("You clicked on row:",row);
+                         console.log("You clicked on col:",col);
+                         console.log("You clicked on item #:",i);
+                         console.log("Cell value",selected.innerHTML);
+                         
+                         el.className='clicked';
+                         if (lastClicked) lastClicked.className='';
+                         lastClicked = el;
+                         });
+
+document.body.appendChild(gameBoard);
+
+var tx = document.createElement("P");
+tx.innerHTML = "Click me!";
+tx.addEventListener("click",function(){
+                    tx.innerHTML = "Thanks!";
+                    });
+tx.style.textAlign = 'center';
+document.body.appendChild(tx);
 
 //initialize game board
-var board = [
-    ['','⚪','','⚪','','⚪','','⚪'],
-    ['⚪','','⚪','','⚪','','⚪',''],
-    ['','⚪','','⚪','','⚪','','⚪'],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['⚫','','⚫','','⚫','','⚫',''],
-    ['','⚫','','⚫','','⚫','','⚫'],
-    ['⚫','','⚫','','⚫','','⚫','',]
-];
+var boardState = [
+             ['','w','','w','','w','','w'],
+             ['w','','w','','w','','w',''],
+             ['','w','','w','','w','','w'],
+             ['','','','','','','',''],
+             ['','','','','','','',''],
+             ['b','','b','','b','','b',''],
+             ['','b','','b','','b','','b'],
+             ['b','','b','','b','','b','',]
+             ];
 
-const init = () => {
-    drawBoard();
-}
+console.log(gameBoard);
+drawBoard(boardState);
+console.log(boardState);
+console.log(gameBoard);
 
-const drawBoard = () => {
-    let ul = document.createElement("ul");
-    let anchor = document.getElementById("board");
-    let list = anchor.appendChild(ul);
-
-    for(row in board) {
-        console.log("On row: " + row);
-        let li = document.createElement("li");
-        let currentList = list.appendChild(li);
-
-        for(piece in board[row]) {
-            let DOMpiece = document.createElement("p");
-            DOMpiece.id = [row,piece];
-            console.log("Piece: " + piece);
-            console.log(board[row], piece);
-            
-            switch(board[row][piece]) {
-                case '⚫':
-                    console.log("Black piece!");
-                    DOMpiece.innerHTML = "[b]";
-                    break;
-                case '⚪':
-                    console.log("White piece!");
-                    DOMpiece.innerHTML = "[w]";
-                    break;
-                default:
-                    console.log("Empty piece!");
-                    DOMpiece.innerHTML = "[_]";
-                    break;
+function createBoard( rows, cols, callback ){
+    var i=0;
+    var board = document.createElement('table');
+    board.className = 'board';
+    board.id = 'board';
+    board.style.border = "thin solid #000";
+    for (var r=0;r<rows;++r){
+        var tr = board.appendChild(document.createElement('tr'));
+        for (var c=0;c<cols;++c){
+            var square = tr.appendChild(document.createElement('td'));
+            square.id = ++i;
+            square.style.width = "40px";
+            square.style.height = "40px";
+            square.style.textAlign = "center";
+            square.style.color = "white";
+            var data = square.appendChild(document.createTextNode('adsf'));
+            if(((c%2==0) && (r%2==1))||((c%2==1) && (r%2==0))) {
+                square.style.backgroundColor = "black";
             }
-
-            currentList.appendChild(DOMpiece);
+            square.addEventListener('click',(function(el,r,c,i){
+                                            return function(){
+                                                callback(el,r,c,i);
+                                            }
+                                           })(square,r,c,i),false);
         }
     }
-    console.log("done");
+    return board;
 }
 
-const moves = () => {
-    movePieces();
-}
-
-const movePieces = () => {
-    //Get piece coordinates clicked on
-
-    
+function drawBoard(state) {
+    var board = document.getElementById("board");
+    var a = 1;
+    for(var i = 0,row; row = board.rows[i]; i++) {
+        // go through rows
+        for(var j = 0,col; col = row.cells[j]; j++) {
+            // go through cells
+            switch(state[i][j]) {
+                    case 'w':
+                        row.cells[j].innerHTML = 'w';
+                        break;
+                    case 'b':
+                        row.cells[j].innerHTML = 'b';
+                        break;
+                    default:
+                        row.cells[j].innerHTML = '';
+                        break;
+            }
+            a++;
+        }
+    }
 }
 
 const musicToggle = () => {
     var myAudio = document.getElementById("myAudio");
     var isPlaying = false;
-
+    
     togglePlay(isPlaying);
 }
 
 //Function to toggle playing of music
 function togglePlay(isPlaying) {
-  if (isPlaying) {
-    myAudio.pause()
-  } else {
-    myAudio.play();
-  }
+    if (isPlaying) {
+        myAudio.pause()
+    } else {
+        myAudio.play();
+    }
 };
 
-//Functions to pause/play music
-myAudio.onplaying = function() {
-  isPlaying = true;
-};
-myAudio.onpause = function() {
-  isPlaying = false;
-};
+
+
+
+
+
+
