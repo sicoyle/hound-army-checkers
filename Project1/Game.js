a4e4a1ee0cc2ef1d25be810318287b899e987a91
Rowan Stone <rowan.stone@txstate.edu>
2018-10-28 12:14:55 -0500

    Some unhighlighting functionality added
    Hints will be un-highlighted when invalid black pieces are selected during the first move. Issues with white pieces and moves after the first still persist for now.

--- Project1/Game.js
+++ Project1/Game.js
@@ -16,15 +16,15 @@ var sc1=0;
 var sc2=0;
 
 const world = [
-               ['x','black','x','black','x','black','x','black'],
-               ['black','x','black','x','black','x','black','x'],
-               ['x','black','x','black','x','black','x','black'],
-               ['','x','','x','','x','','x'],
-               ['x','','x','','x','','x',''],
-               ['white','x','white','x','white','x','white','x'],
-               ['x','white','x','white','x','white','x','white'],
-               ['white','x','white','x','white','x','white','x']
-               ];
+    ['x','black','x','black','x','black','x','black'],
+    ['black','x','black','x','black','x','black','x'],
+    ['x','black','x','black','x','black','x','black'],
+    ['','x','','x','','x','','x'],
+    ['x','','x','','x','','x',''],
+    ['white','x','white','x','white','x','white','x'],
+    ['x','white','x','white','x','white','x','white'],
+    ['white','x','white','x','white','x','white','x']
+];
 
 var w;
 
@@ -78,7 +78,7 @@ const drawWorld = () => {
             currentBoard.appendChild(DOMpiece);
         }
     }
-    
+
     console.log("Donezo");
     return anchor;
 };
@@ -96,10 +96,10 @@ const isSelectableChecker = (row,col) => {
     //currentPlayer));
     let rightNotSelf = (world[+row+bias][+col+1] !== currentPlayer);
     let leftNotSelf = (world[+row+bias][+col-1] !== currentPlayer);
-    
+
     let leftOnBoard = ((world[+row+bias][+col-1] === '') || (world[+row+bias][+col-1] === opponentPlayer));
     let rightOnBoard = ((world[+row+bias][+col+1] ==='') || (world[+row+bias][+col+1] === opponentPlayer));
-    
+
     return correctPlayer && (rightNotSelf || leftNotSelf) && (leftOnBoard || rightOnBoard);};
 
 /*
@@ -116,7 +116,7 @@ const selectPiece = (e) => {
     let row = coords[0];
     let col = coords[2];
     console.log("world piece: [%s,%s] === %s", row, col, world[row][col]);
-    
+
     if(isSelectableChecker(row,col)) {
         selectedPiece = requestedPiece;
         document.getElementById('actor').innerHTML = selectedPiece.id;
@@ -148,7 +148,7 @@ const selectPiece = (e) => {
             else
                 leftpossible = (+row + bias) + ',' + null;
             console.log("leftmove coord: ", leftpossible);
-            
+
             if ((+col + 1) !== null) {
                 rightpossible = (+row + bias) + ',' + (+col + 1);
                 rightnode = document.getElementById(rightpossible);
@@ -178,11 +178,11 @@ const selectPiece = (e) => {
             //highlightedSquares.add(rightnode);
             console.log("just hinted: ", highlightedSquares, " highlightedSquares");
             highlightedSquares.forEach(hintSquare);
-            
+
             //leftnode.style.background = 'yellow';
-            
+
             //rightnode.style.background = 'yellow';
-            
+
             w.addEventListener('click', movePiece, {once:true});
         }
     } else {
@@ -218,7 +218,8 @@ const movePiece = (e) => {
     // BLACK
     if (currentPlayer === PLAYER_1) {
         if ((selectedCoords[0] - rowInt === -1) && (Math.abs(selectedCoords[1] - colInt) === 1) &&
-            (world[rowInt][colInt] !== currentPlayer) && (world[rowInt][colInt] !== opponentPlayer) && (world[rowInt][colInt]!== 'x')){
+            (world[rowInt][colInt] !== currentPlayer) && (world[rowInt][colInt] !== opponentPlayer) && (world[rowInt][colInt]!== 'x')
+            && (world[coords[0]][coords[2]] !== world[selectedCoords[0]][selectedCoords[1]])){
             row = coords[0];
             col = coords[2];
             console.log("Moving piece: [%s,%s]", row, col);
@@ -229,7 +230,8 @@ const movePiece = (e) => {
             console.log("Jump over ", rowInt-1, ',', colInt+1 ,'OR', rowInt-1,',', colInt-1,'. Requested Square ID: ', requestedSquare.id);
             // JUMP LEFT (BLACK)
             if (((selectedCoords[0] - rowInt === -2) && (selectedCoords[1] - colInt) === 2) &&
-                (world[rowInt-1][colInt+1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+                (world[rowInt-1][colInt+1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')
+                && (world[coords[0]][coords[2]] !== world[selectedCoords[0]][selectedCoords[1]])) {
                 row = coords[0];
                 col = coords[2];
                 console.log("Moving piece: [%s,%s]", row, col);
@@ -240,7 +242,8 @@ const movePiece = (e) => {
             }
             // JUMP RIGHT (BLACK)
             if (((selectedCoords[0] - rowInt === -2) && (selectedCoords[1] - colInt) === -2) &&
-                (world[rowInt - 1][colInt - 1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+                (world[rowInt - 1][colInt - 1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')
+                && (world[coords[0]][coords[2]] !== world[selectedCoords[0]][selectedCoords[1]])) {
                 row = coords[0];
                 col = coords[2];
                 console.log("Moving piece: [%s,%s]", row, col);
@@ -262,7 +265,8 @@ const movePiece = (e) => {
     // WHITE
     if (currentPlayer === PLAYER_2){
         if ((selectedCoords[0] - rowInt === 1) && (Math.abs(selectedCoords[1] - colInt) === 1) &&
-            (world[rowInt][colInt] !== currentPlayer) && (world[rowInt][colInt] !== opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+            (world[rowInt][colInt] !== currentPlayer) && (world[rowInt][colInt] !== opponentPlayer) && (world[rowInt][colInt] !== 'x')
+            && (world[coords[0]][coords[2]] !== world[selectedCoords[0]][selectedCoords[1]])) {
             row = coords[0];
             col = coords[2];
             console.log("Moving piece: [%s,%s]", row, col);
@@ -273,7 +277,8 @@ const movePiece = (e) => {
             console.log("Jump over ", rowInt+1 ,',', colInt +1 ,'OR', rowInt+1 ,',' , colInt -1);
             // JUMP LEFT (WHITE)
             if (((selectedCoords[0] - rowInt === 2) && (selectedCoords[1] - colInt) === 2) &&
-                (world[rowInt+1][colInt+1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+                (world[rowInt+1][colInt+1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')
+                && (world[coords[0]][coords[2]] !== world[selectedCoords[0]][selectedCoords[1]])) {
                 row = coords[0];
                 col = coords[2];
                 console.log("Moving piece: [%s,%s]", row, col);
@@ -284,7 +289,8 @@ const movePiece = (e) => {
             }
             // JUMP RIGHT (WHITE)
             if ((selectedCoords[0] - rowInt === 2) && (selectedCoords[1] - colInt === -2) &&
-                (world[rowInt+1][colInt-1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+                (world[rowInt+1][colInt-1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')
+                && (world[coords[0]][coords[2]] !== world[selectedCoords[0]][selectedCoords[1]])) {
                 row = coords[0];
                 col = coords[2];
                 console.log("Moving piece: [%s,%s]", row, col);
@@ -303,35 +309,35 @@ const movePiece = (e) => {
         }
     }
     //console.log("Moving piece: [%s,%s]", row, col);
-    
+
     //requestedSquare.style.background = "red"
     w.addEventListener('click', selectPiece, {once: true});
-    
-    if (isValid && world[row][col] !== 'x') {
+
+    if ((isValid && world[row][col] !== 'x')){ //&& (world[coords[0]][coords[2]] !== world[selectedCoords[0]][selectedCoords[1]])) {
         setTimeout(((playerColor) => {
-                    highlightedSquares.forEach(unhintSquare);
-                    highlightedSquares.clear();
-                    console.log("just unhinted and cleared: ", highlightedSquares, " highlightedSquares");
-                    
-                    console.log("Setting timeout with ", playerColor, " moving into the spot");
-                    selectedPiece.style.background = "dimgray";
-                    selectedPiece.innerHTML = '';
-                    //requestedSquare.style.background = playerColor;
-                    requestedSquare.innerHTML = 'dimgray';
-                    if (playerColor.substr(0, 1) === 'w') {
-                    requestedSquare.innerHTML = '&#9898';
-                    }
-                    if (playerColor.substr(0, 1) === 'b') {
-                    requestedSquare.innerHTML = '&#9899';
-                    }
-                    //requestedSquare.innerHTML = "[" + playerColor.substr(0,1) + "]";
-                    world[row][col] = playerColor;
-                    console.log("selectedcoords: ", selectedCoords);
-                    console.log("selectedCoord[0]:", selectedCoords[0], "; selectedCoords[1]:", selectedCoords[1]);
-                    console.log("world[selectedCoords[0],selectedCoords[1]]:", world[selectedCoords[0]][selectedCoords[1]]);
-                    world[selectedCoords[0]][selectedCoords[1]] = '';
-                    })(currentPlayer), 30000);
-        
+            highlightedSquares.forEach(unhintSquare);
+            highlightedSquares.clear();
+            console.log("just unhinted and cleared: ", highlightedSquares, " highlightedSquares");
+
+            console.log("Setting timeout with ", playerColor, " moving into the spot");
+            selectedPiece.style.background = "dimgray";
+            selectedPiece.innerHTML = '';
+            //requestedSquare.style.background = playerColor;
+            requestedSquare.innerHTML = 'dimgray';
+            if (playerColor.substr(0, 1) === 'w') {
+                requestedSquare.innerHTML = '&#9898';
+            }
+            if (playerColor.substr(0, 1) === 'b') {
+                requestedSquare.innerHTML = '&#9899';
+            }
+            //requestedSquare.innerHTML = "[" + playerColor.substr(0,1) + "]";
+            world[row][col] = playerColor;
+            console.log("selectedcoords: ", selectedCoords);
+            console.log("selectedCoord[0]:", selectedCoords[0], "; selectedCoords[1]:", selectedCoords[1]);
+            console.log("world[selectedCoords[0],selectedCoords[1]]:", world[selectedCoords[0]][selectedCoords[1]]);
+            world[selectedCoords[0]][selectedCoords[1]] = '';
+        })(currentPlayer), 30000);
+
         if (!isJump) {
             currentPlayer = (currentPlayer == PLAYER_1) ? PLAYER_2 : PLAYER_1;
             opponentPlayer = (opponentPlayer == PLAYER_2) ? PLAYER_1 : PLAYER_2;
@@ -361,8 +367,8 @@ const movePiece = (e) => {
             captured = (capturedCoords[0]) + ',' + (capturedCoords[1]);
             capturedNode = document.getElementById(captured);
             capturedNode.innerHTML = '';
-            
-            
+
+
             prevSelectedPiece[0] = rowInt;
             prevSelectedPiece[1] = colInt;
         }
@@ -373,28 +379,35 @@ const movePiece = (e) => {
         //bias *= -1;
         //document.getElementById('player').innerHTML = currentPlayer;
         //console.log("now ", currentPlayer, "'s turn!");
-        
+
         //if(isJump) {
         //    while (selectedCoords !== oldSelectedPiece) {
         //        console.log("You can only move the last jumped piece!");
         //        w.addEventListener('click', selectPiece, {once: true});
         //    }
         //}
-        
+
+    }
+    else{
+        if(!isValid || (world[row][col] !== world[selectedCoords[0]][selectedCoords[1]])){
+            highlightedSquares.forEach(unhintSquare);
+            highlightedSquares.clear();
+            console.log("just unhinted and cleared: ", highlightedSquares, " highlightedSquares");
+        }
     }
 };
 
 
 
 const startTheClock = () => {
-    
+
     setInterval(()=>{
-                let player = ((currentPlayer == PLAYER_1) ? 'p1sc' : 'p2sc');
-                console.log("player waiting " + currentPlayer);
-                let shotclock = document.getElementById(player);
-                shotclock.innerHTML = (player == 'p1sc') ? sc1++ : sc2++;
-                },1000)
-    
+        let player = ((currentPlayer == PLAYER_1) ? 'p1sc' : 'p2sc');
+        console.log("player waiting " + currentPlayer);
+        let shotclock = document.getElementById(player);
+        shotclock.innerHTML = (player == 'p1sc') ? sc1++ : sc2++;
+    },1000)
+
 };
 
 const init = () => {
@@ -411,14 +424,14 @@ var x;
 var button = document.getElementsByClassName('collapsible');
 for (x = 0; x < button.length; x++) {
     button[x].addEventListener("click", function () {
-                               this.classList.toggle("active");
-                               var content = this.nextElementSibling;
-                               if (content.style.maxHeight) {
-                               content.style.maxHeight = null;
-                               } else {
-                               content.style.maxHeight = content.scrollHeight + "px";
-                               }
-                               });
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
 
 function refreshPage(){
