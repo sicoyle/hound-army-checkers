69921fe258ff8c5a118505a92ed89587054c42cf
Rowan Stone <rowan.stone@txstate.edu>
2018-10-27 21:53:11 -0500

    Add files via upload

--- Game.js
+++ Game.js
@@ -1,21 +1,29 @@
 const PLAYER_1='black';
 const PLAYER_2='white';
 var currentPlayer=PLAYER_1;
+var opponentPlayer=PLAYER_2;
 var bias=1;
+var requestedPiece;
+var coords;
 var selectedPiece;
+var prevSelectedPiece = [];
+var isJump = false;
+var isValid;
 var highlightedSquares = new Set();
+var player1Score = 0;
+var player2Score = 0;
 var sc1=0;
 var sc2=0;
 
 const world = [
-    ['','black','','black','','black','','black'],
-    ['black','','black','','black','','black',''],
-    ['','black','','black','','black','','black'],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['white','','white','','white','','white',''],
-    ['','white','','white','','white','','white'],
-    ['white','','white','','white','','white','']
+    ['x','black','x','black','x','black','x','black'],
+    ['black','x','black','x','black','x','black','x'],
+    ['x','black','x','black','x','black','x','black'],
+    ['','x','','x','','x','','x'],
+    ['x','','x','','x','','x',''],
+    ['white','x','white','x','white','x','white','x'],
+    ['x','white','x','white','x','white','x','white'],
+    ['white','x','white','x','white','x','white','x']
 ];
 
 var w;
@@ -45,18 +53,25 @@ const drawWorld = () => {
             switch(world[row][column]){
                 case 'white':
                     console.log("White piece!");
-                    DOMpiece.innerHTML = "[w]";
-                    DOMpiece.style.background = "white";
+                    //DOMpiece.innerHTML = "[w]";
+                    DOMpiece.innerHTML = '&#9898';
+                    DOMpiece.style.background = "dimgray";
                     break;
                 case 'black':
                     console.log("Black piece!");
-                    DOMpiece.innerHTML = "[b]";
-                    DOMpiece.style.background = "black";
+                    //DOMpiece.innerHTML = "[b]";
+                    DOMpiece.innerHTML = '&#9899';
+                    DOMpiece.style.background = "dimgray";
+                    break;
+                case 'x':
+                    console.log("Illegal space!");
+                    DOMpiece.innerHTML = '';
+                    DOMpiece.style.background = '#e0f5ea';
                     break;
                 default:
                     console.log("empty piece!");
-                    DOMpiece.innerHTML = "[_]";
-                    DOMpiece.style.background = "red";
+                    DOMpiece.innerHTML = '';
+                    DOMpiece.style.background = "dimgray";
                     break;
             }
             //currentList.appendChild(DOMpiece);
@@ -70,28 +85,97 @@ const drawWorld = () => {
 
 const logError = (err) => console.error(err);
 
-const isSelectableChecker = (row,col) => { return (world[row][col] == currentPlayer) ? true : false; };
+// isSelectableChecker  takes row and column value and returns if current player location matches coordinates on board AND
+// there is at least one available space to move.
+//const isSelectableChecker = (row,col) => { return (world[row][col] === currentPlayer) ? true : false;};
+const isSelectableChecker = (row,col) => {
+    let correctPlayer = (world[row][col] === currentPlayer);
+    //let rightNotSelf = ((world[+row+bias][+col-1] === currentPlayer) && (world[+row+bias][+col+1] === '')); //!==
+        //currentPlayer));
+    //let leftNotSelf = ((world[+row+bias][+col-1] !== currentPlayer) && (world[+row+bias][+col+1] === ''));
+        //currentPlayer));
+    let rightNotSelf = (world[+row+bias][+col+1] !== currentPlayer);
+    let leftNotSelf = (world[+row+bias][+col-1] !== currentPlayer);
+
+    let leftOnBoard = ((world[+row+bias][+col-1] === '') || (world[+row+bias][+col-1] === opponentPlayer));
+    let rightOnBoard = ((world[+row+bias][+col+1] ==='') || (world[+row+bias][+col+1] === opponentPlayer));
+
+    return correctPlayer && (rightNotSelf || leftNotSelf) && (leftOnBoard || rightOnBoard);};
+
+    /*
+    return ((world[row][col] === currentPlayer) &&
+    (((world[+row+bias][+col-1] === currentPlayer) && (world[+row+bias][+col+1] !==
+    currentPlayer)) || ((world[+row+bias][+col-1] !== currentPlayer) && (world[+row+bias][+col+1] ===
+    currentPlayer))) && (((world[+row+bias][+col-1] === '') || (world[+row+bias][+col+1]) === ''))) ;};
+    */
 
 const selectPiece = (e) => {
-
     requestedPiece = e.target;
-    let coords = requestedPiece.id;
+    coords = requestedPiece.id;
     console.log("requestedPiece: ", requestedPiece, "; coords: ", coords);
     let row = coords[0];
     let col = coords[2];
     console.log("world piece: [%s,%s] === %s", row, col, world[row][col]);
 
-    if(isSelectableChecker(row,col)){
+    if(isSelectableChecker(row,col)) {
         selectedPiece = requestedPiece;
         document.getElementById('actor').innerHTML = selectedPiece.id;
-        let leftpossible = (+row+bias)+','+(+col-1);
-        console.log("leftmove coord: ",leftpossible);
-        let rightpossible = (+row+bias)+','+(+col+1);
-        console.log("rightmove coord: ",rightpossible);
-        let leftnode = document.getElementById(leftpossible);
-        let rightnode = document.getElementById(rightpossible);
-        highlightedSquares.add(leftnode);
-        highlightedSquares.add(rightnode);
+        let leftpossible;
+        let rightpossible;
+        let leftnode;
+        let rightnode;
+        if ((+row + bias) !== null) {
+            if ((+col - 1) !== null) {
+                leftpossible = (+row + bias) + ',' + (+col - 1);
+                leftnode = document.getElementById(leftpossible);
+                if((world[+row+bias][+col-1] !== currentPlayer) && (world[+row+bias][+col-1] !== opponentPlayer)) {
+                    highlightedSquares.add(leftnode);
+                }
+                // Implement left jump highlighting
+                // WHITE
+                if((world[+row+bias][+col-1] === opponentPlayer) && (world[+row+bias-1][+col-2] ==='') && (opponentPlayer === PLAYER_1)){
+                    leftpossible = (+row+bias-1) + ',' + (+col-2);
+                    leftnode = document.getElementById(leftpossible);
+                    highlightedSquares.add(leftnode);
+                }
+                // BLACK
+                if((world[+row+bias][+col-1] === opponentPlayer) && (world[+row+bias+1][+col-2] ==='') && (opponentPlayer === PLAYER_2)){
+                    leftpossible = (+row+bias+1) + ',' + (+col-2);
+                    leftnode = document.getElementById(leftpossible);
+                    highlightedSquares.add(leftnode);
+                }
+            }
+            else
+                leftpossible = (+row + bias) + ',' + null;
+            console.log("leftmove coord: ", leftpossible);
+
+            if ((+col + 1) !== null) {
+                rightpossible = (+row + bias) + ',' + (+col + 1); 
+                rightnode = document.getElementById(rightpossible);
+                if((world[+row+bias][+col+1] !== currentPlayer) && (world[+row+bias][+col+1] !== opponentPlayer)){
+                    highlightedSquares.add(rightnode);
+                }
+                // Implement right jump highlighting
+                // BLACK
+                if((world[+row+bias][+col+1] === opponentPlayer) && (world[+row+bias+1][+col+2] ==='') && (opponentPlayer === PLAYER_2)){
+                    rightpossible = (+row+bias+1) + ',' + (+col+2);
+                    rightnode = document.getElementById(rightpossible);
+                    highlightedSquares.add(rightnode);
+                }
+                // WHITE
+                if((world[+row+bias][+col+1] === opponentPlayer) && (world[+row+bias-1][+col+2] ==='') && (opponentPlayer === PLAYER_1)){
+                    rightpossible = (+row+bias-1) + ',' + (+col+2);
+                    rightnode = document.getElementById(rightpossible);
+                    highlightedSquares.add(rightnode);
+                }
+            }
+            else
+                rightpossible = (+row + bias) + ',' + null;
+            console.log("rightmove coord: ", rightpossible);
+        //let leftnode = document.getElementById(leftpossible);
+        //let rightnode = document.getElementById(rightpossible);
+        //highlightedSquares.add(leftnode);
+        //highlightedSquares.add(rightnode);
         console.log("just hinted: ", highlightedSquares, " highlightedSquares");
         highlightedSquares.forEach(hintSquare);
 
@@ -100,7 +184,7 @@ const selectPiece = (e) => {
         //rightnode.style.background = 'yellow';
 
         w.addEventListener('click', movePiece, {once:true});
-
+       }
     } else {
         logError("nonselectable piece");
         //selectedPiece = false;
@@ -110,49 +194,193 @@ const selectPiece = (e) => {
 
 
 
-const hintSquare = (s) => s.style.background = 'yellow';
+const hintSquare = (s) => {
+    if(s !== null)
+        s.style.background = 'yellow';
+};
 
-const unhintSquare = (s) => s.style.background = 'red';
-
-
-
+const unhintSquare = (s) => {
+    if(s !== null)
+        s.style.background = 'dimgray';
+};
 
 const movePiece = (e) => {
-    requestedSquare = e.target;
-    let coords = requestedSquare.id;
-    let selectedCoords = [selectedPiece.id[0],selectedPiece.id[2]];
+    let requestedSquare = e.target;
+    let coords = requestedSquare.id; // returns a string, therefor coords is also a string value.
+    let rowInt = parseInt(coords[0]);
+    let colInt = parseInt(coords[2]);
+    let row;
+    let col;
+    let selectedCoords = [selectedPiece.id[0], selectedPiece.id[2]];
+    let capturedCoords = [];
     console.log("movePiece: ", requestedSquare, "; coords: ", coords);
-    let row = coords[0];
-    let col = coords[2];
-    console.log("Moving piece: [%s,%s]", row, col);
+    // BLACK
+    if (currentPlayer === PLAYER_1) {
+        if ((selectedCoords[0] - rowInt === -1) && (Math.abs(selectedCoords[1] - colInt) === 1) &&
+            (world[rowInt][colInt] !== currentPlayer) && (world[rowInt][colInt] !== opponentPlayer) && (world[rowInt][colInt]!== 'x')){
+            row = coords[0];
+            col = coords[2];
+            console.log("Moving piece: [%s,%s]", row, col);
+            isValid = true;
+            isJump = false;
+        }
+        else {
+            console.log("Jump over ", rowInt-1, ',', colInt+1 ,'OR', rowInt-1,',', colInt-1,'. Requested Square ID: ', requestedSquare.id);
+            // JUMP LEFT (BLACK)
+            if (((selectedCoords[0] - rowInt === -2) && (selectedCoords[1] - colInt) === 2) &&
+               (world[rowInt-1][colInt+1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+                row = coords[0];
+                col = coords[2];
+                console.log("Moving piece: [%s,%s]", row, col);
+                capturedCoords[0] = rowInt-1;
+                capturedCoords[1] = colInt+1;
+                isValid = true;
+                isJump = true;
+            }
+            // JUMP RIGHT (BLACK)
+            if (((selectedCoords[0] - rowInt === -2) && (selectedCoords[1] - colInt) === -2) &&
+                (world[rowInt - 1][colInt - 1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+                row = coords[0];
+                col = coords[2];
+                console.log("Moving piece: [%s,%s]", row, col);
+                capturedCoords[0] = rowInt-1;
+                capturedCoords[1] = colInt-1;
+                isValid = true;
+                isJump = true;
+            }
+            else {
+                if(!isValid) {
+                    console.log("Move is not valid!");
+                    isValid = false;
+                    isJump = false;
+                }
+            }
+        }
+    }
+    // WHITE
+    if (currentPlayer === PLAYER_2){
+        if ((selectedCoords[0] - rowInt === 1) && (Math.abs(selectedCoords[1] - colInt) === 1) &&
+            (world[rowInt][colInt] !== currentPlayer) && (world[rowInt][colInt] !== opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+            row = coords[0];
+            col = coords[2];
+            console.log("Moving piece: [%s,%s]", row, col);
+            isValid = true;
+            isJump = false;
+        }
+        else {
+            console.log("Jump over ", rowInt+1 ,',', colInt +1 ,'OR', rowInt+1 ,',' , colInt -1);
+            // JUMP LEFT (WHITE)
+            if (((selectedCoords[0] - rowInt === 2) && (selectedCoords[1] - colInt) === 2) &&
+                (world[rowInt+1][colInt+1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+                row = coords[0];
+                col = coords[2];
+                console.log("Moving piece: [%s,%s]", row, col);
+                capturedCoords[0] = rowInt+1;
+                capturedCoords[1] = colInt+1;
+                isValid = true;
+                isJump = true;
+            }
+            // JUMP RIGHT (WHITE)
+            if ((selectedCoords[0] - rowInt === 2) && (selectedCoords[1] - colInt === -2) &&
+                (world[rowInt+1][colInt-1] === opponentPlayer) && (world[rowInt][colInt] !== 'x')) {
+                row = coords[0];
+                col = coords[2];
+                console.log("Moving piece: [%s,%s]", row, col);
+                capturedCoords[0] = rowInt+1;
+                capturedCoords[1] = colInt-1;
+                isValid = true;
+                isJump = true;
+            }
+            else {
+                if (!isValid) {
+                    console.log("Move is not valid!");
+                    isValid = false;
+                    isJump = false;
+                }
+            }
+        }
+    }
+    //console.log("Moving piece: [%s,%s]", row, col);
 
     //requestedSquare.style.background = "red"
+    w.addEventListener('click', selectPiece, {once: true});
 
-    w.addEventListener('click', selectPiece, {once:true});
+    if (isValid && world[row][col] !== 'x') {
+        setTimeout(((playerColor) => {
+            highlightedSquares.forEach(unhintSquare);
+            highlightedSquares.clear();
+            console.log("just unhinted and cleared: ", highlightedSquares, " highlightedSquares");
 
-    setTimeout(((playerColor) => {
-        highlightedSquares.forEach(unhintSquare);
-        highlightedSquares.clear();
-        console.log("just unhinted and cleared: ", highlightedSquares, " highlightedSquares");
+            console.log("Setting timeout with ", playerColor, " moving into the spot");
+            selectedPiece.style.background = "dimgray";
+            selectedPiece.innerHTML = '';
+            //requestedSquare.style.background = playerColor;
+            requestedSquare.innerHTML = 'dimgray';
+            if (playerColor.substr(0, 1) === 'w') {
+                requestedSquare.innerHTML = '&#9898';
+            }
+            if (playerColor.substr(0, 1) === 'b') {
+                requestedSquare.innerHTML = '&#9899';
+            }
+            //requestedSquare.innerHTML = "[" + playerColor.substr(0,1) + "]";
+            world[row][col] = playerColor;
+            console.log("selectedcoords: ", selectedCoords);
+            console.log("selectedCoord[0]:", selectedCoords[0], "; selectedCoords[1]:", selectedCoords[1]);
+            console.log("world[selectedCoords[0],selectedCoords[1]]:", world[selectedCoords[0]][selectedCoords[1]]);
+            world[selectedCoords[0]][selectedCoords[1]] = '';
+        })(currentPlayer), 30000);
 
-        console.log("Setting timeout with ", playerColor, " moving into the spot");
-        selectedPiece.style.background = "red";
-        selectedPiece.innerHTML = "[_]";
-        requestedSquare.style.background = playerColor;
-        requestedSquare.innerHTML = "[" + playerColor.substr(0,1) + "]";
-        world[row][col] = playerColor;
-        console.log("selectedcoords: ", selectedCoords);
-        console.log("selectedCoord[0]:",selectedCoords[0],"; selectedCoords[1]:",selectedCoords[1]);
-        console.log("world[selectedCoords[0],selectedCoords[1]]:",world[selectedCoords[0]][selectedCoords[1]]);
-        world[selectedCoords[0]][selectedCoords[1]] = '';
-    })(currentPlayer), 30000);
+        if (!isJump) {
+            currentPlayer = (currentPlayer == PLAYER_1) ? PLAYER_2 : PLAYER_1;
+            opponentPlayer = (opponentPlayer == PLAYER_2) ? PLAYER_1 : PLAYER_2;
+            bias *= -1;
+            document.getElementById('player').innerHTML = currentPlayer;
+            console.log("now ", currentPlayer, "'s turn!");
+            w.addEventListener('click', selectPiece, {once: true});
+        }
+        else {
+            let captured;
+            let capturedNode;
+            // update player score
+            if (currentPlayer === PLAYER_1) {
+                player1Score++;
+                console.log("Player 1 (Black) Score: ", player1Score);
+                let p1Score = document.getElementById("player1-score");
+                p1Score.textContent = player1Score;
+            }
+            if (currentPlayer === PLAYER_2) {
+                player2Score++;
+                console.log("Player 1 (Black) Score: ", player2Score);
+                let p2Score = document.getElementById("player2-score");
+                p2Score.textContent = player2Score;
+            }
+            // remove captured piece from board
+            world[capturedCoords[0]][capturedCoords[1]] = '';
+            captured = (capturedCoords[0]) + ',' + (capturedCoords[1]);
+            capturedNode = document.getElementById(captured);
+            capturedNode.innerHTML = '';
 
-    // Validation move functionality should go in movePiece
-    currentPlayer = (currentPlayer == PLAYER_1) ? PLAYER_2 : PLAYER_1;
-    bias*=-1;
-    document.getElementById('player').innerHTML = currentPlayer;
-    console.log("now ", currentPlayer, "'s turn!");
-}
+
+            prevSelectedPiece[0] = rowInt;
+            prevSelectedPiece[1] = colInt;
+        }
+            // check for another jump...
+            selectedCoords = [parseInt(selectedPiece.id[0]), parseInt(selectedPiece.id[2])];
+            console.log(typeof selectedCoords[0]);
+            console.log(typeof prevSelectedPiece[0]);
+            //bias *= -1;
+            //document.getElementById('player').innerHTML = currentPlayer;
+            //console.log("now ", currentPlayer, "'s turn!");
+
+            //if(isJump) {
+            //    while (selectedCoords !== oldSelectedPiece) {
+            //        console.log("You can only move the last jumped piece!");
+            //        w.addEventListener('click', selectPiece, {once: true});
+            //    }
+            //}
+
+    }
+};
 
 
 
